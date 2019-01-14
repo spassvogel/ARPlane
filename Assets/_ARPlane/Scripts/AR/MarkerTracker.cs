ab13cb06fae55f645131e15dd690e4bc9bf15364
Remi <remialkemade@gmail.com>
2019-01-14 22:09:24 +0100

    MarkerTracker correctly positions SessionOrigin!

--- Assets/_ARPlane/Scripts/AR/MarkerTracker.cs
+++ Assets/_ARPlane/Scripts/AR/MarkerTracker.cs
@@ -10,13 +10,20 @@
     using PaperPlaneTools.AR;
 
     public class MarkerTracker : WebCamera {
+		[Tooltip("World offset")]
 		public Transform offsetObject;
+		[Tooltip("Player camera")]
 		public Transform playerObject;
+		[Tooltip("(0-1) Slows down AR tracking movement to stabilize position and rotation.")]
+		public float stabilization = 0.8f;
+		public float stabilizationThreshold = 1f;
 
 		/// <summary>
 		/// The marker detector
 		/// </summary>
 		private MarkerDetector markerDetector;
+
+		private List<Listener> listeners = new List<Listener>();
 
 		void Start () {
 			Debug.Assert(offsetObject != null, "ARCamera: No offsetObject set.");
@@ -24,10 +31,14 @@
 			markerDetector = new MarkerDetector ();
 		}
 
+		public void AddListener(Listener listener) {
+			this.listeners.Add(listener);
+		}
+
 		protected override void ReadTextureConversionParameters() {
-			// We DONT want it to flip!
 			base.ReadTextureConversionParameters();
 #if UNITY_EDITOR
+			// We DONT want it to flip!
 			TextureParameters.FlipHorizontally = false;
 #endif
 		}
@@ -62,44 +73,57 @@
 			if(arucoIds.Count == 0) {
 				return;
 			}
+			FireMarkerDetected();
 
 			Matrix4x4 transformMatrix = markerDetector.TransfromMatrixForIndex(0);
 			PositionPlayer(transformMatrix);
 		}
 
+
 		private void PositionPlayer(Matrix4x4 transformMatrix) {
 			Matrix4x4 matrixY = Matrix4x4.TRS (Vector3.zero, Quaternion.identity, new Vector3 (1, -1, 1));
 			Matrix4x4 matrixZ = Matrix4x4.TRS (Vector3.zero, Quaternion.identity, new Vector3 (1, 1, -1));
-			Matrix4x4 matrix = matrixY * transformMatrix * matrixZ;
+			Matrix4x4 matrix = matrixZ * transformMatrix.inverse * matrixY;
 
-			Vector3 markerPosition = MatrixHelper.GetPosition(matrix);
-			Quaternion markerRotation = MatrixHelper.GetQuaternion(matrix);
+			Vector3 targetPosition = MatrixHelper.GetPosition(matrix);
+			Quaternion targetRotation = MatrixHelper.GetQuaternion(matrix);
 
-			// First fix rotation error because it can mess up the position
-
-			Quaternion playerRotation = playerObject.transform.rotation;
-			// The player rotation should be the inverse of the marker rotation.
-			Quaternion targetRotation = Quaternion.Inverse(markerRotation);
+			Quaternion playerRotation = playerObject.rotation;
 			Quaternion errorRotation = targetRotation * Quaternion.Inverse(playerRotation);
-			
-			Vector3 mRotation = markerRotation.eulerAngles;
-			Vector3 tRotation = targetRotation.eulerAngles;
-			Vector3 eRotation = errorRotation.eulerAngles;
-			Vector3 fRotation = (errorRotation * playerRotation).eulerAngles;
-
-			// Rotate offset to counter error
-			offsetObject.transform.rotation = errorRotation * offsetObject.transform.rotation;
-
-			// Now fix position error
 
 			Vector3 playerPosition = playerObject.position;
-			// The marker is placed at some point from the origin. We want the marker to be the origin, so player should be (0,0,0) - markerPosition.
-			Vector3 targetPosition = Vector3.zero - markerPosition;
 			Vector3 errorPosition = targetPosition - playerPosition;
 
 			// Move offset to counter error
-			offsetObject.transform.position += errorPosition;
+			Vector3 move = (1-stabilization) * errorPosition;
+			float distance = move.magnitude;
+			offsetObject.position += move;
 
+			// Rotate offset to counter error
+			Quaternion offsetRotation = Quaternion.Lerp(Quaternion.identity, errorRotation, 1-stabilization) * offsetObject.rotation;
+			float rotationAngle = Quaternion.Angle(offsetObject.rotation, offsetRotation);
+			offsetObject.rotation = offsetRotation;
+
+			if(distance < 0.1f * stabilizationThreshold && rotationAngle < 0.5f * stabilizationThreshold) {
+				FireMarkerStable();
+			}
+		}
+
+		private void FireMarkerDetected() {
+			foreach(Listener listener in listeners) {
+				listener.OnMarkerDetected();
+			}
+		}
+
+		private void FireMarkerStable() {
+			foreach(Listener listener in listeners) {
+				listener.OnMarkerStabilized();
+			}
+		}
+
+		public interface Listener {
+			void OnMarkerDetected();
+			void OnMarkerStabilized();
 		}
 	}
 }
