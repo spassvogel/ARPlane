2f80f81c6460d20fecc89ab71c24a40d63ba74aa
Remi <info@remialkemade.nl>
2020-05-08 22:32:30 +0200

    WIP: adding ARReferenceManager

---
+++ Assets/_ARPlane/Scripts/ARMarkerReferenceManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[System.Serializable]
+public class ARReferencePosition {
+    public string markerName;
+    public Vector3 position;
+}
+
+public class ARMarkerReferenceManager : MonoBehaviour {
+    public ARReferencePosition[] realWorldPositions;
+    
+    public void ReferenceMarkerDetected(ARReferenceMarker marker) {
+
+    }
+}
