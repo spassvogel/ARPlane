19339b6824f1cf206e4aa5ea82aa970b43f074f8
Remi <info@remialkemade.nl>
2020-05-10 21:50:31 +0200

    Tweaked & improved flying physics

--- Assets/_ARPlane/Scripts/ARCraft.cs
+++ Assets/_ARPlane/Scripts/ARCraft.cs
@@ -1,138 +1,150 @@
 ﻿//#define DEBUG
 
 namespace UniversoAumentado.ARCraft {
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Net;
+	using UnityEngine;
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+	[RequireComponent(typeof(Rigidbody))]
+	public class ARCraft : MonoBehaviour {
 
-[RequireComponent(typeof(Rigidbody))]
-public class ARCraft : MonoBehaviour {
+		public Transform target;
+		public Transform cam;
+		public DebugTexts debugTexts;
 
-	public Transform target;
-	public Transform cam;
-	public float maxForce = 200f;
-	public float maxForceDistance = 0.2f;
-	public float forwardForceBias = 5f;
-	public float rotateSpeed = .5f;
-	public float stability = 30f;
-	public float banking = 30f;
-	public float lift = 500f;
-	public float noseDownForce = 30f;
-	public float artificialGravity = 250f;
-	public float expectedMaxSpeed = 30f;
+		private float maxForce = 500;
+		private float maxForceDistance = 25f;
+		private float forwardForceBias = 5f;
+		private float rotateSpeed = .5f;
+		private float stability = 30f;
+		private float banking = 30f;
+		private float lift = 500f;
+		private float noseDownForce = 30f;
+		private float artificialGravity = 250f;
+		private float expectedMaxSpeed = 200f;
 
-	private Vector3 previousVelocity;
-	private float currentLift = 0f;
+		public bool reverse = false;
 
-	public bool reverse = false;
+		private Vector3 previousVelocity;
+		private float currentLift = 0f;
+		private float distanceToTarget = 0;
+		private float horizontalDistanceToTarget = 0;
 
-	private float debugValue = 0f;
+		Rigidbody body;
 
-	Rigidbody body;
-
-	// Use this for initialization
-	void Start () {
-		body = GetComponent<Rigidbody>();
-		previousVelocity = body.velocity;
-    }
+		// Use this for initialization
+		void Start () {
+			body = GetComponent<Rigidbody>();
+			previousVelocity = body.velocity;
+		}
 	
-	// Update is called once per frame
-	void Update () {
-		currentLift = CalculateLift(body.velocity);
+		// Update is called once per frame
+		void Update () {
+			CalculateValues();
+			LookAtTarget();
+			FollowTarget();
+			Banking();
+			Stabilize();
+			Gravity();
 
-		LookAtTarget();
-		FollowTarget();
-		Banking();
-		Stabilize();
-		Gravity();
+			previousVelocity = body.velocity;
+		}
 
-		previousVelocity = body.velocity;
-	}
+		void CalculateValues() {
+			distanceToTarget = Vector3.Distance(target.position, transform.position);
+			currentLift = CalculateLift(body.velocity);
+			horizontalDistanceToTarget = Vector2.Distance(
+				new Vector2(target.position.x, target.position.z),
+				new Vector2(transform.position.x, transform.position.z)
+			);
+			DebugText("ARCraft:", $"{transform.position}");
+			DebugText("Target:", $"{target.position}");
+		}
 
-	float  CalculateLift(Vector3 velocity) {
-		Vector3 horizontalVelocity = new Vector3(velocity.x, 0, velocity.z);
-		float magnitude = horizontalVelocity.magnitude;
+		float CalculateLift(Vector3 velocity) {
+			float horizontalSpeed = new Vector2(velocity.x, velocity.z).magnitude;
 
-		// Should move significantly (10%+) to get the plane started again
-		if(magnitude < expectedMaxSpeed / 10f) magnitude = 0;
+			// Should move significantly (10%+) to get the plane started again
+			if (horizontalSpeed < expectedMaxSpeed / 10f) horizontalSpeed = 0;
 
-		return Mathf.Max(0, Mathf.Min(1, magnitude / expectedMaxSpeed));
-	}
+			return Mathf.Max(0, Mathf.Min(1, horizontalSpeed / expectedMaxSpeed));
+		}
 
-	void LookAtTarget() {
-		// Find target position to look at
-		Vector3 aim = (target.position - cam.position).normalized * 10;
-		if(reverse) aim *= -1;
-		Vector3 lookAtTarget = target.position + aim;
-		Vector3 targetDelta = lookAtTarget - transform.position;
+		void LookAtTarget() {
+			// Find target position to look at
+			Vector3 aim = (target.position - cam.position).normalized * 10;
+			if(reverse) aim *= -1;
+			Vector3 lookAtTarget = target.position + aim;
+			Vector3 targetDelta = lookAtTarget - transform.position;
  
-		// Get the angle between transform.forward and target delta
-		float angleDiff = Vector3.Angle(transform.forward, targetDelta);
+			// Get the angle between transform.forward and target delta
+			float angleDiff = Vector3.Angle(transform.forward, targetDelta);
  
-		// Get its cross product, which is the axis of rotation to
-		// Get from one vector to the other
-		Vector3 cross = Vector3.Cross(transform.forward, targetDelta);
+			// Get its cross product, which is the axis of rotation to
+			// get from one vector to the other
+			Vector3 cross = Vector3.Cross(transform.forward, targetDelta);
  
-		// Apply torque along that axis according to the magnitude of the angle.
-		body.AddTorque(cross * angleDiff * rotateSpeed * currentLift);
+			// Apply torque along that axis according to the magnitude of the angle.
+			body.AddTorque(cross * angleDiff * rotateSpeed * currentLift);
 
-		Debug.DrawLine(transform.position, lookAtTarget, Color.green);
-	}
+			Debug.DrawLine(transform.position, lookAtTarget, Color.green);
+		}
 
-	void FollowTarget() {
-		if(target == null) return;
+		void FollowTarget() {
+			if(target == null) return;
 
-		float distance = Vector3.Distance(target.position, transform.position);
-		Vector3 direction = (target.position - transform.position).normalized;
+			Vector3 direction = (target.position - transform.position).normalized;
 
-        // Attract
-		float distanceMultiplier = Mathf.Min(1, distance / maxForceDistance);
-		Vector3 engineForce = direction * distanceMultiplier * maxForce;
-        body.AddForce(engineForce);
+			// Attract
+			float distanceMultiplier = Mathf.Min(1, horizontalDistanceToTarget / maxForceDistance);
+			Vector3 engineForce = direction * distanceMultiplier * maxForce;
+			body.AddForce(engineForce);
 
-		// Attract more in Z-direction of target
-		Vector3 fromTarget = target.InverseTransformDirection(target.position - transform.position);
-		Vector3 force = target.TransformDirection(new Vector3(0, 0, fromTarget.z));
-		force.y = 0; // no force bias in y-direction
-		body.AddForce(force * forwardForceBias);
-	}
+			DebugText("Horizontal Distance To Target:", $"{horizontalDistanceToTarget}");
+			DebugText("Distance Multiplier:", $"{distanceMultiplier}");
 
-	void Banking() {
-		Vector3 velocity = transform.InverseTransformDirection(body.velocity);
-		Vector3 acceleration = velocity - transform.InverseTransformDirection(previousVelocity);
+			// Brake
+			//float brakeMultiplier = Mathf.Min(1, 1 - horizontalDistanceToTarget / maxForceDistance);
+			//Vector3 brakeForce = -1 * direction * brakeMultiplier * maxForce * 2;
+			//body.AddForce(brakeForce);
 
-		debugValue = acceleration.x;
-		body.AddRelativeTorque(Vector3.forward * (-acceleration.x) * banking);
-	}
+			// Attract more in Z-direction of target
+			Vector3 fromTarget = target.InverseTransformDirection(target.position - transform.position);
+			Vector3 force = target.TransformDirection(new Vector3(0, 0, fromTarget.z));
+			force.y = 0; // no force bias in y-direction
+			body.AddForce(force * forwardForceBias);
+		}
 
-	void Stabilize() {
-		Vector3 torqueVector = Vector3.Cross(transform.up, Vector3.up);
-		body.AddTorque(torqueVector * stability);
-	}
+		void Banking() {
+			Vector3 velocity = transform.InverseTransformDirection(body.velocity);
+			Vector3 acceleration = velocity - transform.InverseTransformDirection(previousVelocity);
 
-	void Gravity() {
-		// Continuous gravity
-		body.AddForce(Vector3.down * artificialGravity);
+			body.AddRelativeTorque(Vector3.forward * (-acceleration.x) * banking);
+		}
 
-		// Lift to cancel out gravity
-		Vector3 velocity = transform.InverseTransformDirection(body.velocity);
-		velocity.z = 0;
-		body.AddForce(Vector3.up * Mathf.Min(artificialGravity, currentLift * lift));
+		void Stabilize() {
+			Vector3 torqueVector = Vector3.Cross(transform.up, Vector3.up);
+			body.AddTorque(torqueVector * stability);
+		}
+
+		void Gravity() {
+			// Continuous gravity
+			body.AddForce(Vector3.down * artificialGravity);
+
+			// Lift to cancel out gravity
+			body.AddForce(Vector3.up * Mathf.Min(artificialGravity, currentLift * lift));
 		
-		// Continuous nose down
-		Vector3 noseDown = Vector3.Cross(transform.forward, Vector3.down);
-		body.AddTorque(noseDown * noseDownForce);
-	}
+			// Continuous nose down
+			Vector3 noseDown = Vector3.Cross(transform.forward, Vector3.down);
+			body.AddTorque(noseDown * noseDownForce);
+		}
 
-#if DEBUG
-    private void OnGUI()
-    {
-        GUIStyle guiStyle = new GUIStyle() { fontSize = 140 };
-        guiStyle.normal.textColor = Color.red;
-        GUI.Label(new Rect(100, 100, 100, 20), "Debug: " + Mathf.Round(debugValue), guiStyle);
-    }
-#endif
-}
+		void DebugText(string name, string value) {
+			if (debugTexts == null) return;
+			debugTexts.SetDebugText(name, value);
+		}
+	}
 
 }
